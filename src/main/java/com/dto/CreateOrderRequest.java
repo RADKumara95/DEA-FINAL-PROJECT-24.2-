d3e84532025bf83189fcafe4427505bda8745cea
hcsdilruk <hcsdilruk@students.nsbm.ac.lk>
2025-11-08 16:08:20 +0530

    [ADD] Create DTOs

--- src/main/java/com/dto/CreateOrderRequest.java
+++ src/main/java/com/dto/CreateOrderRequest.java
@@ -78,36 +78,5 @@ public class CreateOrderRequest {
         this.paymentMethod = paymentMethod;
     }
 
-    public static class OrderItemRequest {
-        @NotNull(message = "Product ID is required")
-        private Long productId;
-
-        @NotNull(message = "Quantity is required")
-        private Integer quantity;
-
-        public OrderItemRequest() {
-        }
-
-        public OrderItemRequest(Long productId, Integer quantity) {
-            this.productId = productId;
-            this.quantity = quantity;
-        }
-
-        public Long getProductId() {
-            return productId;
-        }
-
-        public void setProductId(Long productId) {
-            this.productId = productId;
-        }
-
-        public Integer getQuantity() {
-            return quantity;
-        }
-
-        public void setQuantity(Integer quantity) {
-            this.quantity = quantity;
-        }
-    }
 }
 
