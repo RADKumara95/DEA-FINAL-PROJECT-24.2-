524d46cf846a21a8aec45dc707e51e1fe8248187
hcsdilruk <hcsdilruk@students.nsbm.ac.lk>
2025-11-08 09:23:49 +0530

    [ADD] Create Order Repositories

---
+++ src/main/java/com/model/OrderStatus.java
@@ -0,0 +1,12 @@
+package com.model;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
+
+
---
+++ src/main/java/com/model/PaymentMethod.java
@@ -0,0 +1,12 @@
+package com.model;
+
+
+public enum PaymentMethod {
+    CASH_ON_DELIVERY,
+    CREDIT_CARD,
+    DEBIT_CARD,
+    UPI,
+    NET_BANKING
+}
+
+
